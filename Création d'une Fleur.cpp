0ff98535306689d6e88f2c1d978c5290ed80398d
git-ago <91827304+git-ago@users.noreply.github.com>
2021-10-07 18:27:11 +0200

    Add files via upload

---
+++ Création d'une Fleur.cpp
@@ -0,0 +1,12 @@
+class Fleur
+{
+    public:
+    // Constructeur
+    Fleur(float inTaille, float inPoids, string inIdentifiant, int inNombrePetales)
+    {
+        taille = inTaille;
+        poids = inPoids;
+        identifiant = inIdentifiant;
+        nombrePetales = inNombrePetales;
+    }
+}
